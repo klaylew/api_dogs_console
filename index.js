dd75f5ed2ee9820cc0da047e901f87dd289f8747
Klayton Lewis <64672448+klaylew@users.noreply.github.com>
2020-11-08 23:39:26 -0800

    Outlined changes to change functionality to console output of multiple images

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 'use strict';
 
-function getDogImage() {
-  fetch('https://dog.ceo/api/breeds/image/random')
+function getDogImages() {
+  console.log("Running get dog images")
+  fetch(makeFetch())
     .then(response => response.json())
     .then(responseJson => 
       displayResults(responseJson))
@@ -10,22 +11,40 @@ function getDogImage() {
 
 function displayResults(responseJson) {
   console.log(responseJson);
-  //replace the existing image with the new one
+  /* //replace the existing image with the new one
   $('.results-img').replaceWith(
     `<img src="${responseJson.message}" class="results-img">`
   )
   //display the results section
-  $('.results').removeClass('hidden');
+  $('.results').removeClass('hidden'); */
 }
 
 function watchForm() {
-  $('form').submit(event => {
-    event.preventDefault();
-    getDogImage();
+  $("form").on(click => {
+    console.log("watchForm ran");
+    getDogImages();
   });
+}
+
+//function to create API fetch string with entered number of pics
+function makeFetch(){
+  let address = "https://dog.ceo/api/breeds/image/random/";
+  address = address + $("#number").val();
+  console.log(address);
+  return address;  
 }
 
 $(function() {
   console.log('App loaded! Waiting for submit!');
   watchForm();
 });
+
+
+/** 
+ * 
+ * need to hold data for number of pictures with a default value of 3
+ * 
+ * retrieve photo addresses and print to the console
+ *  
+ * 
+ */
